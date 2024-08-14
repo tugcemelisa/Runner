7e39ce3bba93e2c974df3e0bc88532e164f675ef
Tugce Melisa <tugcehaspolat@gmail.com>
2024-08-14 10:46:15 +0300

    Update: Character Script (character can run, attack, moving between lanes)

--- Runner/Assets/Scripts/CharacterScript.cs
+++ Runner/Assets/Scripts/CharacterScript.cs
@@ -3,29 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterScript : MonoBehaviour
-{    
+{
+    [SerializeField] private string message;
     [SerializeField] Rigidbody rb;
-    [SerializeField] float speed; 
+    [SerializeField] float speed;
 
     void Start()
     {
-        Debug.Log("Character's current position: " + transform.position); // To find the position of an object 
-    }     
-    void Update() 
+        Debug.Log(message); // Kullanýcýnýn mesajýný konsola yazdýrýr
+        Debug.Log("Character's current position: " + transform.position); // Karakterin baþlangýç pozisyonunu konsola yazdýrýr
+    }
+
+    void Update()
     {
+        // 'Space' tuþuna basýldýðýnda karakterin saldýrmasýný saðlar
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+
+        // Karakteri sola hareket ettirir, sadece x pozisyonu -9'dan büyükse
         if (Input.GetKeyDown(KeyCode.A) && transform.position.x > -9)
         {
             transform.Translate(-9, 0, 0);
         }
+
+        // Karakteri saða hareket ettirir, sadece x pozisyonu 9'dan küçükse
         if (Input.GetKeyDown(KeyCode.D) && transform.position.x < 9)
         {
             transform.Translate(9, 0, 0);
         }
-
     }
+
     void FixedUpdate()
     {
-       rb.MovePosition(transform.position + transform.forward * speed * Time.deltaTime);       
+        // Karakterin ileri doðru sabit hýzda hareket etmesini saðlar
+        rb.MovePosition(transform.position + transform.forward * speed * Time.deltaTime);
     }
-   
+
+    void Attack()
+    {
+        // Karakterin saldýrý kodu burada yer alýr
+        Debug.Log("Karakter saldýrýyor!");
+    }
 }
