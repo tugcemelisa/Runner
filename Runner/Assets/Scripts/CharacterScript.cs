fc284fef09aded48e9c495b0d049638fb62c7b6c
Tugce Melisa <tugcehaspolat@gmail.com>
2024-09-07 21:06:21 +0300

    Add: UI Management, Props, Shaders, Sprite Assets, Font, Materials Update: Animations

--- Runner/Assets/Scripts/CharacterScript.cs
+++ Runner/Assets/Scripts/CharacterScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CharacterScript : MonoBehaviour
@@ -8,38 +9,61 @@ public class CharacterScript : MonoBehaviour
     [SerializeField] float speed;
     [SerializeField] float shift;  
     [SerializeField] float jumpForce;  
-    [SerializeField] Animator anim;      
+    [SerializeField] Animator anim; 
+    [SerializeField] TMP_Text score;  
+    [SerializeField] GameObject menu;
+    float roundScore;
+    bool isGameOver;   
 
     void Start()
-    {              
+    {         
+             
     }     
     void Update() 
-    {                   
-        if(Input.GetKeyDown(KeyCode.A) && transform.position.x > -2)
-        {
-            transform.Translate(-shift, 0, 0);
-        }
-        if(Input.GetKeyDown(KeyCode.D) && transform.position.x < 2)
-        {
-            transform.Translate(shift, 0, 0);
-        }
+    {       
+        if(!isGameOver)
+        {    
+            roundScore += Time.deltaTime;
+            score.text = "Score: " + roundScore.ToString("f1");        
+        
+            if(Input.GetKeyDown(KeyCode.A) && transform.position.x > -2)
+            {
+                transform.Translate(-shift, 0, 0);
+            }
+            if(Input.GetKeyDown(KeyCode.D) && transform.position.x < 2)
+            {
+                transform.Translate(shift, 0, 0);
+            }
 
-        if(Input.GetKeyDown(KeyCode.Space) && rb.velocity.y == 0)
-        {
-            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);  
-                              
-        }
-        if(rb.velocity.y > 0)   
-        {
-            anim.SetBool("jump", true);  
-        }
-        if(rb.velocity.y == 0)
-        {
-            anim.SetBool("jump", false); 
-        }        
+            if(Input.GetKeyDown(KeyCode.Space) && rb.velocity.y == 0)
+            {
+                rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);  
+                                
+            }
+            if(rb.velocity.y > 0)   
+            {
+                anim.SetBool("jump", true);  
+            }
+            if(rb.velocity.y == 0)
+            {
+                anim.SetBool("jump", false); 
+            } 
+        }       
     }
     void FixedUpdate()
     {
-       rb.MovePosition(transform.position + transform.forward * speed * Time.deltaTime);       
+        if(!isGameOver)
+        {
+            rb.MovePosition(transform.position + transform.forward * speed * Time.deltaTime);       
+        }
     }   
+    private void OnCollisionEnter(Collision other) 
+    {
+        if(other.gameObject.CompareTag("Obstacle"))
+        {
+            isGameOver = true;            
+            menu.SetActive(true);
+            anim.SetBool("death", true);
+        }
+    }
 }
--- Runner/Assets/Scripts/GameManager.cs
+++ Runner/Assets/Scripts/GameManager.cs
@@ -1,34 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] List<GameObject> locations = new List<GameObject>();
+    [SerializeField] List<GameObject> locations = new List<GameObject>(); 
     [SerializeField] Transform player;
-    float levelLength = 106f;
-    int count = 4;
+    float levelLength = 106.3f;
+    int count = 50;   
 
     void Start()
     {
-        Instantiate(locations[0], transform.forward, transform.rotation);
-        for (int i = 0; i < count; i++)
-        {
+       Instantiate(locations[0], transform.forward, transform.rotation);
+       for(int i = 0; i < count; i++)
+       {
             CreateLocation();
-        }
+       }
     }
 
     void CreateLocation()
     {
         Instantiate(locations[Random.Range(0, locations.Count)], transform.forward * levelLength, transform.rotation);
-        levelLength += 106f;
+        levelLength += 106.3f;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     void Update()
     {
-        if (player.position.z > levelLength - 106f * count)
+        if(player.position.z > levelLength - 106.3f * count)
         {
             CreateLocation();
         }
-    }
+    }  
 }
